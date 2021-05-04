d2e62fc15822a1c1d2d49ed2ba53f980714512d8
Patrick <patrickvieiramota@gmail.com>
2021-05-04 16:15:18 -0300

    Change json

--- index.js
+++ index.js
@@ -16,14 +16,43 @@ app.get("/api", async(req, res) => {
 
     let response = await axios.get('https://api.github.com/orgs/takenet/repos')
 
-    let repos = {"status": "ok", "results": []}
+    let repos = {
+        id: '',
+        type: "application/vnd.lime.collection+json",
+        to: "128271320123982@messenger.gw.msging.net",
+        content:{
+            itemType: "application/vnd.lime.document-select+json",
+            items: []
+        }
+    }
 
     for(let i in response.data){
         if(response.data[i].language === 'C#'){
-            repos.results.push({"Title": response.data[i].name, "Text": response.data[i].description, "Uri": response.data[i].owner.avatar_url})
+            repos.content.items.push(
+                {   
+                    header:{
+                        type: "application/vnd.lime.media-link+json",
+                        value:{
+                            title: response.data[i].name, 
+                            text: response.data[i].description, 
+                            type: "image/jpeg",
+                            uri: response.data[i].owner.avatar_url  
+                        }
+                    }
+                }
+            )
 
         }
     }
+
+    // let repos = {"status": "ok", "results": []}
+
+    // for(let i in response.data){
+    //     if(response.data[i].language === 'C#'){
+    //         repos.results.push({"Title": response.data[i].name, "Text": response.data[i].description, "Uri": response.data[i].owner.avatar_url})
+
+    //     }
+    // }
 
     return res.json(repos).send('Ok')
  
